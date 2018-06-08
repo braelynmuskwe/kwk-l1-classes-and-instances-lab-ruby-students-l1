66df474ef75ef736e95c1c3ed46d3a2b58fb6142
braelynmuskwe <braemira1@icloud.com>
2018-06-08 23:11:37 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,6 @@
 # person.rb
 class Person
+<<<<<<< HEAD
    def name=(person_name)
     @this_person_name = person_name
   end 
@@ -14,3 +15,7 @@ alan_kay=Person.new
 alan_kay.name="alan_kay"
 puts "Her name is #{adele_goldberg.name}"
 puts "His name is #{alan_kay.name}"
+=======
+  
+  
+>>>>>>> 78bf937e018eb148fb0ece03e87b442945f2ebec
