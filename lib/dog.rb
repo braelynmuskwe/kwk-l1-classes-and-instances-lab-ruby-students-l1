3c42391e214d31b5680658ba6b8240462d89dc73
braelynmuskwe <braemira1@icloud.com>
2018-06-08 17:22:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # dog.rb
+class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+    end 
+    
+    def name
+      @this_dogs_name
+    end
+  end 
+    
+  fido=Dog.new
+  fido.name="fido"
+  lassie=Dog.new
+  lassie.name="lassie"
+  snoopy=Dog.new
+  snoopy.name="snoopy"
+  
+  puts "The dogs name is #{fido.name}"
+  puts "The dogs name is #{lassie.name}"
+  puts "The dogs name is #{snoopy.name}"
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # person.rb
+class Person
+   def name=(person_name)
+    @this_person_name = person_name
+  end 
+  def name 
+    @this_person_name
+  end
+end
+
+adele_goldberg=Person.new
+adele_goldberg.name="adele_goldberg"
+alan_kay=Person.new
+alan_kay.name="alan_kay"
+puts "Her name is #{adele_goldberg.name}"
+puts "His name is #{alan_kay.name}"
